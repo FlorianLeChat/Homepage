f54b10138040f54a9c6f9339bdf3c0311665081f
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2024-05-20 20:41:12 +0200

    Fixed various issues with Svelte files related to ESLint and Prettier

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [svelte(), basicSsl()],
-});
+export default defineConfig( {
+	plugins: [ svelte(), basicSsl() ]
+} );
