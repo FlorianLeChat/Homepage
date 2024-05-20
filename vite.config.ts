78e1dcf099ef3f8e04096d136c2ce361a009c32f
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2024-05-20 20:28:34 +0200

    Added some Svelte/Vite plugins only active in development environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import basicSsl from "@vitejs/plugin-basic-ssl";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-})
+	plugins: [svelte(), basicSsl()],
+});
