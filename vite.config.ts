9d0811dbc6c2893e81a891283024715daa134b89
Florian Trayon <git@florian-trayon.fr>
2024-05-25 14:59:37 +0200

    Added ESLint support for Svelte and JavaScript configuration files

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import basicSsl from "@vitejs/plugin-basic-ssl";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig( {
 	plugins: [ svelte(), basicSsl() ]
 } );
