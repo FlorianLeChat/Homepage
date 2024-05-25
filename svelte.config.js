2c3de717606a6faa2b834806824e6a8ae968f673
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2024-05-25 10:13:53 +0200

    Removed unused plugins installed automatically by Svelte

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,5 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 export default {
-	preprocess: vitePreprocess(),
-	vitePlugin: {
-		inspector: {
-			toggleKeyCombo: "control-shift",
-			toggleButtonPos: "bottom-left",
-			showToggleButton: "always",
-		},
-	},
+	preprocess: vitePreprocess()
 };
