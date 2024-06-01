51ed2ab7588bcec25b798d8061cab97bcb18a131
Florian Trayon <git@florian-trayon.fr>
2024-06-01 18:48:14 +0200

    Removed duplicated and unused CSS style rules since latest changes

--- src/commands/home.ts
+++ src/commands/home.ts
@@ -16,7 +16,7 @@ export default [
 	"#    Welcome to Raven Orange 2024.06.1 LTS (GNU/Linux 5.15.0-72-generic x86_64)             #",
 	"#    An Open Source Operating System made with love by Raven Industries © 2024              #",
 	"#                                                                                           #",
-	"#     * Documentation:  <link=\"https://docs.raven.com/systems/orange-2024.06.1-lts/\">https://docs.raven.com/systems/orange-2024.06.1-lts/</link>                #",
+	"#     * Documentation:  <link=\"https://docs.raven.com/systems/orange-2024.06.1-lts\">https://docs.raven.com/systems/orange-2024.06.1-lts</link>                 #",
 	"#     * Management:     <link=\"https://admin.raven.com/?id=1e0f7d42-8b39-4c7b-9d2a-03b14f3e7e5b\">https://admin.raven.com/?id=1e0f7d42-8b39-4c7b-9d2a-03b14f3e7e5b</link>    #",
 	"#     * Support:        <link=\"https://support.raven.com/?product=raven-orange-2024.06.1-lts\">https://support.raven.com/?product=raven-orange-2024.06.1-lts</link>       #",
 	"#                                                                                           #",
