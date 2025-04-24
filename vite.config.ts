3c55208030910630203cd804c3a4663005ff89a0
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2025-04-24 08:29:14 +0200

    Removed pointless npm dependencies for Svelte or TypeScript

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import basicSsl from "@vitejs/plugin-basic-ssl";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { defineConfig } from "vite";
 
 export default defineConfig( {
 	base: "",
-	plugins: [ svelte(), basicSsl() ]
+	plugins: [ svelte() ]
 } );
